1ffcbdafda24f0cca943425ae0d1942813e94974
glaubermarcelino <gmsoft.informatica@gmail.com>
2019-09-26 21:10:40 -0300

    Adicionado LoginPage e alterado background

---
+++ ReclameJa/ReclameJa/Views/LoginPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ManFree.ViewModels;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms;
+
+namespace ReclameJa.Views
+{
+    //[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            BindingContext = new LoginViewModel();
+        }
+
+        private void BtnEnter_Clicked(object sender, System.EventArgs e)
+        {
+            //App.Current.MainPage = new NavigationPage(new MainPage());
+            App.Current.MainPage = new NavigationPage(new DashBoard());
+        }
+
+        private async void BtnPopup_Clicked(object sender, System.EventArgs e)
+        {
+            //Chamado ao Rg.Plugins.Popup
+            await PopupNavigation.Instance.PushAsync(new PopUpFreePage());
+
+        }
+    }
+}
